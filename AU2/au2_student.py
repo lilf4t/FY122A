1c5af21cb6fce745480a3f649d854942ad4b0f2e
Fatima Kadum <112803504+lilf4t@users.noreply.github.com>
2024-10-07 22:50:07 +0200

    Update au2_student.py

--- AU2/au2_student.py
+++ AU2/au2_student.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# @author - Fatima Kadum, Laila Suleiman, Alice Jeverdeanu
 
 ###################################  Uppgift 1, differentialekvation ##########################
 
